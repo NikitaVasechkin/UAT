514858fea456ec61d0a6a3d7befde81f452e5edd
Vasechkin_Nikita <vaseckin.nik@gmail.com>
2023-10-11 17:51:02 +0300

    Actually now added the py file with initial requerements

---
+++ Ultimate_accuracy_test.py
@@ -0,0 +1,17 @@
+'''
+Ultimate accuracy test (UAT)
+Goal: easen the accuracy testing via human-visual and aritificially aided tests.
+
+0. Upload the UAT to your github.
+
+1. Implement easy to use visual inspecting tool for assesing the accuracy of depth map
+    a) Provide easy to access validation data
+    b) Provide method to upload and use tested data
+    c) Create the accessable view of comparison
+    d) Implement segmented view (if applicable)
+
+2. Implement the tool and metrics to validate the data
+    a) Import already used techniques
+    b) Search and add new techniques
+
+'''
